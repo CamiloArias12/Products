5242c77093d5892754f5d1650d64693538f7063c
CamiloArias12 <ariaspira13@gmail.com>
2024-11-24 21:10:25 -0500

    feat:add order dto

---
+++ src/main/java/com/example/products/dto/order/OrderRes.java
@@ -0,0 +1,34 @@
+package com.example.products.dto.order;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.products.model.Order;
+import com.example.products.model.Product;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderRes {
+    private int id;
+    public Product product;
+
+
+    public static OrderRes fromEntity(Order order) {
+        return new OrderRes(
+                order.getId(),
+                order.getProduct()
+                );
+    }
+
+    public static List<OrderRes> fromEntities(List<Order> orders) {
+        List<OrderRes> ordersRes = new ArrayList<>();
+        for (Order order : orders) {
+            ordersRes.add(fromEntity(order));
+        }
+        return ordersRes;
+    }
+}
